1306126fabb73baa51039f6a2b7e5c4e17d4a774
Jacob Birkeland <birkeland02@gmail.com>
2021-11-16 11:19:07 -0600

    updated root reducer to include sat, sun, and favs

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import fridayReducer from './friday.reducer';
+import saturdayReducer from './saturday.reducer';
+import sundayReducer from './sunday.reducer';
+import favoritesReducer from './favorites.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -13,6 +16,9 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   fridayReducer, 
+  saturdayReducer,
+  sundayReducer,
+  favoritesReducer,
 });
 
 export default rootReducer;
