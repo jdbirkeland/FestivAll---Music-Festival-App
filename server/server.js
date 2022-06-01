35a0150e01bad55357132208c830a90913aebbb3
Jacob Birkeland <85916564+jdbirkeland@users.noreply.github.com>
2022-05-31 19:12:09 -0500

    update

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/api/favorites', favoritesRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(PORT, () => {
